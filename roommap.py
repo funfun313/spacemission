e07d20420d4cc214c91f72156a2a6b15a263aef2
funfun313 <43829418+funfun313@users.noreply.github.com>
2020-01-14 20:35:37 -0600

    Add files via upload

--- roommap.py
+++ roommap.py
@@ -19,7 +19,7 @@ toplefty = 150
 #OBJECT_LIST = [images.floor,images.pillar,images.soil]
 roomheight = 0
 roomwidth = 0
-currentroom = 31
+currentroom = 32
 #########################
 #### Player Variables ###
 #########################
@@ -39,6 +39,7 @@ def draw():
     roomheight = len(roommap)
     roomwidth = len(roommap[0])
 
+    screen.clear()
     for y in range(roomheight):
         for x in range(roomwidth):
             item = roommap[y][x]
@@ -47,7 +48,7 @@ def draw():
     drawplayer()
 
 def drawplayer():
-    print (playerframe)
+    #print (playerframe)
     playerimage = PLAYER[playerdirection][playerframe]
     screen.blit(playerimage,(topleftx+ playerx*30,toplefty + playery*30-playerimage.get_height()))
 def autogenroom(roomnum):
@@ -58,6 +59,18 @@ def autogenroom(roomnum):
     height = GAME_MAP[roomnum][1]
     exittop = GAME_MAP[roomnum][3]
     exitright = GAME_MAP[roomnum][4]
+    exitbottom = False
+    exitleft = False
+    #bottom exit
+    if roomnum < 46:
+        if GAME_MAP[roomnum+5][3]:
+            exitbottom = True
+
+    #left exit
+    if roomnum %5 != 1:
+        if GAME_MAP[roomnum-1][4]:
+            exitleft = True
+
     #rooms 1-25
     if roomnum < 26:
         if roomnum < 6:
@@ -114,31 +127,50 @@ def autogenroom(roomnum):
         if exitright:
             rightexit = int(height/2)
         else:
-            rightexit = -5
+            rightexit = -99
+        if exitleft:
+            leftexit = int(height/2)
+        else:
+            leftexit = -99
         for i in range(height -2):
-            temprow = [1]
+            if i == leftexit or i == leftexit - 1 or i == leftexit + 1:
+                temprow = [0]
+            else:
+                temprow = [1]
             temprow = temprow + [0] * (width - 2)
             if i == rightexit or i == rightexit - 1 or i == rightexit + 1:
                 temprow = temprow + [0]
             else:
                 temprow = temprow + [1]
             temproommap.append(temprow)
-        temprow = [1] * width
-        temproommap.append(temprow)
+        #bottom row
+        if exitbottom:
+            temprow = [1] * int(width / 2-1)
+            temprow = temprow + [0]*2
+            temprow = temprow + [1] * (width - len(temprow))
+            temproommap.append(temprow)
+        else:
+            temprow = [1] * width
+            temproommap.append(temprow)
     #########################
     ##     Add Scenery     ##
     #########################
-    roomscenery =  scenery[roomnum]
-    for b in roomscenery:
-        temproommap[b[1]][b[2]] = b[0]
+    if roomnum in scenery:
+        roomscenery =  scenery[roomnum]
+        for b in roomscenery:
+            temproommap[b[1]][b[2]] = b[0]
 
 
     roommap = temproommap
+    global roomheight
+    global roomwidth
+    roomheight = len(roommap)
+    roomwidth = len(roommap[0])
 def gameLoop():
     global currentroom
     global playerx, playery, playerdirection, playerframe, playerimage, playeroffsetx, playeroffsety, fromplayerx
     global fromplayery
-
+    global roomwidth
     if playerframe > 0:
         playerframe += 1
         if playerframe == 5:
@@ -173,11 +205,18 @@ def gameLoop():
         #print(playery)
 
         #moving between rooms
-        print(playerx, playery, roomwidth, roomheight)
+        #print(playerx, playery, roomwidth, roomheight)
         if playerx == roomwidth:
             currentroom += 1
             autogenroom(currentroom)
-            playerx = 1 #zero once left side doors
+            playerx = 0 #zero once left side doors
+            playery = int(roomheight / 2)
+            #print("HI")
+            playerframe = 0
+        if playerx == -1:
+            currentroom -= 1
+            autogenroom(currentroom)
+            playerx = roomwidth-1
             playery = int(roomheight / 2)
             #print("HI")
             playerframe = 0
@@ -185,7 +224,14 @@ def gameLoop():
             currentroom -= 5
             autogenroom(currentroom)
             playerx = int(roomwidth / 2)
-            playery = roomheight - 2
+            playery = roomheight - 1
+            playerframe = 0
+        if playery == roomheight:
+            currentroom += 5
+            autogenroom(currentroom)
+            playerx = int(roomwidth / 2)
+            playery = 1
+            print(playery)
             playerframe = 0
         if roommap[playery][playerx] not in items_player_may_stand_on:
 
